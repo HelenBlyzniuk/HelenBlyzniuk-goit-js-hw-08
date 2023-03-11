78cd0c9ec7ec444094d4c77c5cbc0d9418539a61
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-03-11 17:34:13 +0300

    smth on player have been done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+import throttle from "lodash.throttle";   
+import Player from "@vimeo/player";
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+const timeUpdate = function (time) {
+    localStorage.setItem('videoplayer-current-time');
+}
